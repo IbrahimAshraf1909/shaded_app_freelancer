f689b513548151bfeb24aea6b798d1309aa98c53
Ibrahim Alhmida <ibr.ashr.0@gmail.com>
2023-07-24 14:18:23 +0300

    commit2

---
+++ lib/captain_app/captain_home.dart
@@ -0,0 +1,17 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class CaptainHome extends StatelessWidget {
+  const CaptainHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: const Text('Home page'),),
+      body: Container(color: Colors.red,),
+    );
+  }
+}
---
+++ lib/user_app/user_home.dart
@@ -0,0 +1,17 @@
+
+
+
+
+import 'package:flutter/material.dart';
+
+class UserHome extends StatelessWidget {
+  const UserHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(title: Text('Home')),
+      body: Container(),
+    );
+  }
+}
